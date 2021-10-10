ab27b74130de6a80407772aef1ee3f1c0a6f74ba
markk39006 <markk39006@gmail.com>
2021-10-10 22:00:18 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+
+enum Menu
+{
+    RegisterNewStudent = 1,
+    RegisterNewTeacher,
+    GetListPersons
+}
+
+enum selectActivity
+{
+    ComputerProgram = 1,
+    GradeA
+}
+
+namespace Employee
+{
+    class Program
+    {
+        static PersonList personList;
+
+        static void Main(string[] args)
+        {
+            PreparePersonListWhenProgramIsLoad();
+            PrintMenuScreen();
+        }
+
+        static void PreparePersonListWhenProgramIsLoad()
+        {
+            Program.personList = new PersonList();
+        }
+
+        static void PrintMenuScreen()
+        {
+            Console.Clear();
+            PrintHeader();
+            PrintListMenu();
+            InputMenuFromKeyboard();
+        }
+
+        static void PrintHeader()
+        {
+            Console.WriteLine("Welcome to registration new user school application.");
+            Console.WriteLine("----------------------------------------------------");
+        }
+
+        static void PrintListMenu()
+        {
+            Console.WriteLine("1. Register new student.");
+            Console.WriteLine("2. Register new Teacher.");
+            Console.WriteLine("3. Get List Persons.");
+        }
+
+        static void InputMenuFromKeyboard()
+        {
+            Console.Write("Please Select Menu: ");
+            Menu menu = (Menu)(int.Parse(Console.ReadLine()));
+
+            PresentMenu(menu);
+        }
+
+        static void PresentMenu(Menu menu)
+        {
+            if (menu == Menu.RegisterNewStudent)
+            {
+                ShowInputRegisterNewStudentScreen();
+            }
+            else if (menu == Menu.RegisterNewTeacher)
+            {
+                ShowInputRegisterNewTeacherScreen();
+            }
+            else if (menu == Menu.GetListPersons)
+            {
+                ShowGetListPersonScreen();
+            }
+            else
+            {
+                ShowMessageInputMenuIsInCorrect();
+            }
+        }
+
+        static void ShowInputRegisterNewStudentScreen()
+        {
+            Console.Clear();
+            PrintHeaderRegisterStudent();
+
+            int totalStudent = TotalNewStudents();
+            InputNewStudentFromKeyboard(totalStudent);
+        }
+
+        static void ShowInputRegisterNewTeacherScreen()
+        {
+            Console.Clear();
+            PrintHeaderRegisterTeacher();
+
+            int totalTeacher = TotalNewTeacher();
+            InputNewTeacherFromKeyboard(totalTeacher);
+        }
+
+        static void ShowGetListPersonScreen()
+        {
+            Console.Clear();
+            Program.personList.FetchPersonsList();
+            InputExitFromKeyboard();
+        }
+
+        static void InputExitFromKeyboard()
+        {
+            string text = "";
+            while (text != "exit")
+            {
+                Console.WriteLine("Input: ");
+                text = Console.ReadLine();
+            }
+
+            Console.Clear();
+            PrintMenuScreen();
+        }
+
+        static void InputNewTeacherFromKeyboard(int totalTeacher)
+        {
+            for (int i = 0; i < totalTeacher; i++)
+            {
+                Console.Clear();
+                PrintHeaderRegisterTeacher();
+
+                Teacher teacher = CreateNewTeacher();
+                Program.personList.AddNewPerson(teacher);
+            }
+
+            Console.Clear();
+            PrintMenuScreen();
+        }
+
+        static void InputNewStudentFromKeyboard(int totalStudent)
+        {
+            for (int i = 0; i < totalStudent; i++)
+            {
+                Console.Clear();
+                PrintHeaderRegisterStudent();
+
+                Student student = CreateNewStudent();
+                Program.personList.AddNewPerson(student);
+            }
+
+            Console.Clear();
+            PrintMenuScreen();
+        }
+
+        static Student CreateNewStudent()
+        {
+            return new Student(InputName(),
+             InputAddress(),
+             InputCitizenID(),
+             InputStudentID(), InputActivityName());
+
+        }
+
+        static Teacher CreateNewTeacher()
+        {
+            return new Teacher(InputName(),
+            InputAddress(),
+            InputCitizenID(),
+            InputEmployeeID(),InputActivityName());
+        }
+
+        static string InputActivityName()
+        {
+            string A = "";
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Select activity");
+            Console.WriteLine("1. Learning for Computer Programming Grade A");
+            Console.WriteLine("2. Grade A me if you can");
+            Console.Write("Select activity : ");
+
+            selectActivity selectactivity = (selectActivity)(int.Parse(Console.ReadLine()));
+            if(selectactivity == selectActivity.ComputerProgram)
+            {
+                A = "Learning for Computer Programming Grade A";
+            }
+            else if(selectactivity == selectActivity.GradeA)
+            {
+                A = "Grade A me if you can";
+            }
+            return A;
+        }
+
+        static string InputName()
+        {
+            Console.Write("Name: ");
+
+            return Console.ReadLine();
+        }
+
+        static string InputStudentID()
+        {
+            Console.Write("Student ID: ");
+
+            return Console.ReadLine();
+        }
+
+        static string InputAddress()
+        {
+            Console.Write("Address: ");
+
+            return Console.ReadLine();
+        }
+
+        static string InputCitizenID()
+        {
+            Console.Write("Citizen ID: ");
+
+            return Console.ReadLine();
+        }
+
+        static string InputEmployeeID()
+        {
+            Console.Write("Employee ID: ");
+
+            return Console.ReadLine();
+        }
+
+        static int TotalNewStudents()
+        {
+            Console.Write("Input Total new Student: ");
+
+            return int.Parse(Console.ReadLine());
+        }
+
+        static int TotalNewTeacher()
+        {
+            Console.Write("Input Total new Teacher: ");
+
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void PrintHeaderRegisterStudent()
+        {
+            Console.WriteLine("Register new student.");
+            Console.WriteLine("---------------------");
+        }
+
+        static void PrintHeaderRegisterTeacher()
+        {
+            Console.WriteLine("Register new teacher.");
+            Console.WriteLine("---------------------");
+        }
+
+        static void ShowMessageInputMenuIsInCorrect()
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Incorrect Please try again.");
+            InputMenuFromKeyboard();
+        }
+    }
+
+    class Person
+    {
+        protected string name;
+        protected string address;
+        protected string citizenID;
+        protected string Activity;
+
+        public Person(string name, string address, string citizenID, string activity)
+        {
+            this.name = name;
+            this.address = address;
+            this.citizenID = citizenID;
+            this.Activity = activity;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+        public string GetActivity()
+        {
+            return this.Activity;
+        }
+    }
+
+    class PersonList
+    {
+        private List<Person> personList;
+
+        public PersonList()
+        {
+            this.personList = new List<Person>();
+        }
+
+        public void AddNewPerson(Person person)
+        {
+            this.personList.Add(person);
+        }
+
+        public void FetchPersonsList()
+        {
+            Console.WriteLine("List Persons");
+            Console.WriteLine("---------------------");
+            foreach (Person person in this.personList)
+            {
+                if (person is Student)
+                {
+                    Console.WriteLine("Name: {0}", person.GetName());
+                    Console.WriteLine("Type: Student");
+                    Console.WriteLine("Activity : {0}", person.GetActivity());
+                }
+                else if (person is Teacher)
+                {
+                    Console.WriteLine("Name: {0}", person.GetName());
+                    Console.WriteLine("Type: Teacher");
+                    Console.WriteLine("Activity : {0}", person.GetActivity());
+                }
+            }
+        }
+    }
+
+    class Student: Person 
+    { 
+        private string studentID;
+
+        public Student(string name, string address, string citizenID, string studentID,string activity): base(name, address, citizenID,activity)
+        {
+            this.studentID = studentID;
+        }
+        static int TotalNewStudents()
+        {
+            Console.Write("Input Total new Student: ");
+
+            return int.Parse(Console.ReadLine());
+        }
+    }
+
+    class Teacher : Person
+    {
+        private string employeeID;
+
+        public Teacher(string name, string address, string citizenID, string employeeID,string activity)
+        : base(name, address, citizenID,activity)
+        {
+            this.employeeID = employeeID;
+        }
+    }
+}
